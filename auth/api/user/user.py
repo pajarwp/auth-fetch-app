be3d71f16373f6a6156c0196b03f182287b3b835
pajarwp <pwputra@alterra.id>
2021-12-02 03:23:58 +0700

    feat(api): add auth fields validation
    Add validation for numeric phonenumber with min lenght 8 and max 12, length password must be 4, and all field must be string
    
    Closes #19

--- auth/api/user/user.py
+++ auth/api/user/user.py
@@ -1,6 +1,7 @@
 from flask import Blueprint
 from flask_restful import Api, Resource, reqparse
 from flask_jwt_extended import create_access_token, jwt_required, get_jwt_identity
+from flask_expects_json import expects_json
 from database import get_connection
 from util.response import create_response
 import datetime, random, string
@@ -12,19 +13,34 @@ class User(Resource):
     def __init__(self):
         self.db = get_connection()
         
+    schema = {
+        'type': 'object',
+        'properties': {
+            'phone': {'type': 'string', 'minLength':8, 'maxLength':12},
+            'name': {'type': 'string'},
+            'role': {'type': 'string'}
+        },
+    'required': ['phone', 'name', 'role']
+    }
+        
     @jwt_required
     def get(self):
         current_user = get_jwt_identity()
         return create_response(current_user, "Success", "ok"), 200
-        
+    
+    @expects_json(schema)    
     def post(self):
         parse = reqparse.RequestParser()
         parse.add_argument('phone', location='json', type=str, required=True)
         parse.add_argument('name', location='json', type=str, required=True)
         parse.add_argument('role', location='json', type=str, required=True)
         args = parse.parse_args()
+        
+        check = args["phone"].isnumeric()
+        if check == False:
+            return create_response([], "Phone must be number", "failed")
                 
-        user = self.db.execute("SELECT name FROM user WHERE name=" + args["name"]).fetchone()
+        user = self.db.execute("SELECT name FROM user WHERE name='" + args["name"] + "'").fetchone()
         if user != None:
             return create_response([], "Name already used, use different name", "failed")
         
@@ -54,13 +70,27 @@ class UserToken(Resource):
     def __init__(self):
         self.db = get_connection()
         
+    schema = {
+        'type': 'object',
+        'properties': {
+            'phone': {'type': 'string', 'minLength':8, 'maxLength':12},
+            'password': {'type': 'string', 'minLength':4, 'maxLength':4},
+        },
+    'required': ['phone', 'password']
+    }
+    
+    @expects_json(schema)  
     def post(self):
         parse = reqparse.RequestParser()
         parse.add_argument('phone', location='json', type=str, required=True)
         parse.add_argument('password', location='json', type=str, required=True)
         args = parse.parse_args()
         
-        user = self.db.execute("SELECT name, role, created_at FROM user WHERE phone=" + args["phone"] + " AND password='" + args["password"] + "'").fetchone()
+        check = args["phone"].isnumeric()
+        if check == False:
+            return create_response([], "Phone must be number", "failed")
+        
+        user = self.db.execute("SELECT name, role, created_at FROM user WHERE phone='" + args["phone"] + "'" + " AND password='" + args["password"] + "'").fetchone()
         if user == None:
             return create_response([], "Invalid credential, please check phone number and password", "Unauthorized"), 401
         claims= {
--- auth/app.py
+++ auth/app.py
@@ -1,8 +1,9 @@
-from flask import Flask
+from flask import Flask, make_response, jsonify
 from flask_jwt_extended import JWTManager
 from database import init_db
 import os
 from dotenv import load_dotenv
+from jsonschema import ValidationError
 
 def create_app():
     app = Flask(__name__)
@@ -10,6 +11,7 @@ def create_app():
     load_dotenv()
     app.config["JWT_SECRET_KEY"] = os.getenv("JWT_SECRET")
     app.config["JWT_TOKEN_LOCATION"] = 'headers'
+    app.register_error_handler(400, handle_bad_request)
     JWTManager(app)
     
     init_db()
@@ -18,6 +20,12 @@ def create_app():
     app.register_blueprint(bp_user, url_prefix='/user')
 
     return app
+
+def handle_bad_request(error):
+    if isinstance(error.description, ValidationError):
+        original_error = error.description
+        return make_response(jsonify({'error': original_error.message}), 400)
+    return error
 
 if __name__ == "__main__":
     app = create_app()
